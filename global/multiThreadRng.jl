76a9153122359c03f0b3e6c4a8a9a4fd1ec8f4a6
GuandiZhao <grinderzhao@gmail.com>
2024-10-08 08:17:41 +0200

    add multithread rng to global lib

---
+++ global/multiThreadRng.jl
@@ -0,0 +1,10 @@
+using Threads
+using Random
+
+function multiThreadRng(n::Int)
+    rngs = [MersenneTwister() for i in 1:n]
+    @threads for i in 1:n
+        Random.seed!(rngs[i], i)
+        println(rand(rngs[i]))
+    end
+end
